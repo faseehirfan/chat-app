93649e5529d6b7a0a8221ec22a7ebdd5807bf2d0
Faseeh Irfan <faseeh_irfan@outlook.com>
2023-01-10 15:37:36 -0500

    set up basic server

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("API is running");
+})
+app.listen(5000, console.log("Server started on PORT 5000"));
