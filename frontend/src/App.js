a6d6c8e84d87ac868af89dd31b7bf95b126941c6
Faseeh Irfan <faseeh_irfan@outlook.com>
2023-01-13 00:37:51 -0500

    set up chakra ui and fetching data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import { Route } from 'react-router-dom';
 import './App.css';
+import ChatPage from './pages/ChatPage';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <div className="App">
-      <Route path = '/' />
+      <Route path = '/' component={Homepage} exact />
+      <Route path = '/chats' component={ChatPage}/>
     </div>
   );
 }
---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Homepage
