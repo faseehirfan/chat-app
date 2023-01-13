fb905ea71936a8de233ba40770558ff908046c24
Faseeh Irfan <faseeh_irfan@outlook.com>
2023-01-13 01:19:06 -0500

    added user schema

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -8,6 +8,6 @@ const messageModel = mongoose.Schema({
     timestamps: true,
 });
 
-const Message = mongoose.model("Message, messageModel");
+const Message = mongoose.model("Message", messageModel);
 
 module.exports = Message;
