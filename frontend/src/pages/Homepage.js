e551fe9e071b46199632b563853f5e79a7e9079f
Faseeh Irfan <faseeh_irfan@outlook.com>
2023-01-13 19:10:57 -0500

    added tabs to hp and background

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import React from 'react';
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
+
 
 const Homepage = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  return <Container maxW="xl" centerContent>
+    <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px">
+      <Text fontSize="4xl" fontFamily="Work sans">
+        Chatty
+      </Text>
+    </Box>
+
+    <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+      <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width = "50%">Login</Tab>
+            <Tab width = "50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+    </Box>
+  </Container>;
+  
+};
 
 export default Homepage
