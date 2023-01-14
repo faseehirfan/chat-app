25a14cc8b11b881a9360b9a6f3434996bb29bcfd
Faseeh Irfan <faseeh_irfan@outlook.com>
2023-01-13 20:31:38 -0500

    created signup tab

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -24,13 +24,13 @@ const Homepage = () => {
         m="40px 0 15px 0"
         borderRadius="lg"
         borderWidth="1px">
-      <Text fontSize="4xl" fontFamily="Work sans">
+      <Text fontSize="4xl" fontFamily="Work sans" textAlign="center">
         Chatty
       </Text>
     </Box>
 
-    <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
-      <Tabs isFitted variant="soft-rounded">
+    <Box bg="white" w="100%" p={4} borderRadius="lg" color = "black" borderWidth="1px">
+      <Tabs isFitted variant="soft-rounded" colorScheme="purple">
           <TabList mb="1em">
             <Tab width = "50%">Login</Tab>
             <Tab width = "50%">Sign Up</Tab>
