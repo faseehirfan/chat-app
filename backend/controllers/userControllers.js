1e9f584496234fe6a7fc8f641c8910c36e23a8ee
Faseeh Irfan <faseeh_irfan@outlook.com>
2023-01-14 13:50:48 -0500

    login functionality implemented in userController.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -44,10 +44,10 @@ const registerUser = asyncHandler(async (req,res) => {
 
 
 const authUser = asyncHandler(async(req,res) => {  //login functionality. 
-    const { email, password } = request.body;
+    const { email, password } = req.body;
 
     const user = await User.findOne({ email });
-    if (user && () ) {
+    if (user && (await user.matchPassword(password)) ) {
             res.json({
             _id: user._id,
             name: user.name,
@@ -60,4 +60,4 @@ const authUser = asyncHandler(async(req,res) => {  //login functionality.
         throw new Error("Invalid Email or Password");
     }
 });
-module.exports = { registerUser };
+module.exports = { registerUser, authUser };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs'); //using bcryptjs dependency to encrypt password 
 
 const userSchema = mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true ,unique: true},
     password: { type: String, required: true },
-    picture: {
+    pic: {
         type: String,
         default:
             "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
@@ -13,5 +14,17 @@ const userSchema = mongoose.Schema({
     timestamps: true
 });
 
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 const User = mongoose.model("User", userSchema);
 module.exports = User;
