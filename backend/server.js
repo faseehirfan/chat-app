c965aacf9ae733a8f30fdb5d0b69e11603d73702
Faseeh Irfan <faseeh_irfan@outlook.com>
2023-01-15 23:37:35 -0500

    Added access, fetch, create gc, rename gc, remove from gc, and add to gc apis

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 
@@ -17,7 +18,9 @@ app.get('/', (req, res) => {
     res.send("API is running");
 })
 
-app.use('/api/user', userRoutes);    
+app.use('/api/user', userRoutes);  
+app.use('/api/chat', chatRoutes);  
+  
 
 app.use(notFound);
 app.use(errorHandler);
