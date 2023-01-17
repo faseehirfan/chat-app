3caf45e493491b5db29fb8821a1287a7d6b3e02a
Faseeh Irfan <faseeh_irfan@outlook.com>
2023-01-16 20:09:38 -0500

    commented out history.push in chatprovider as it was casuing an error

--- frontend/src/Context/chatProvider.js
+++ frontend/src/Context/chatProvider.js
@@ -1,23 +1,25 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
+
 const ChatContext = createContext();
 
 const ChatProvider = ({ children }) => {
   const [selectedChat, setSelectedChat] = useState();
   const [user, setUser] = useState();
   const [notification, setNotification] = useState([]);
-  const [chats, setChats] = useState([]);
+  const [chats, setChats] = useState();
 
   const history = useHistory();
 
-  useEffect(() => {
-    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-    setUser(userInfo);
 
-    if (!userInfo) history.push("/");
-    
-  }, [history]);
+  // useEffect(() => {
+  //   const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  //   setUser(userInfo);
+
+  //   if (!userInfo) history.push('/chats');
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [history]);
 
   return (
     <ChatContext.Provider
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -26,7 +26,7 @@ const Homepage = () => {
   
   return (<Container maxW="xl" centerContent>
     <Box
-        d="flex"
+        display="flex"
         justifyContent="center"
         p={3}
         bg="white"
@@ -59,4 +59,4 @@ const Homepage = () => {
   );
 };
 
-export default Homepage
+export default Homepage;
