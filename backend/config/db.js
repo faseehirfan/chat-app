b67b4c197bbb507f805530f6b8a808ddb68d6ea3
Faseeh Irfan <faseeh_irfan@outlook.com>
2024-03-31 17:02:06 -0700

    restart mongodb cluster

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
-mongoose.set('strictQuery',false);
+mongoose.set("strictQuery", false);
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-        console.log(`MonogoDB Connected: ${conn.connection.host}`.cyan.underline);
-    } catch (error) {
-        console.log(`Error: ${error.message}`.red.bold);
-        process.exit();
-    }
+    console.log(`MonogoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.log(`Error: ${error.message}`.red.bold);
+    process.exit();
+  }
 };
 module.exports = connectDB;
