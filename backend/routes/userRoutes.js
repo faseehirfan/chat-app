d258cc8b0a38f16d700a9128b0dd3b56cd6a8440
Faseeh Irfan <faseeh_irfan@outlook.com>
2024-03-31 18:50:58 -0700

    add error to output

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const { registerUser, authUser, allUsers } = require("../controllers/userControllers");
-const { protect } = require('../middleware/authMiddleware');
+const express = require("express");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.route("/").post(registerUser).get(protect, allUsers); //protect middleware will run first before moving
-router.post("/login", authUser);                                //on to the allUsers request
+router.post("/login", authUser); //on to the allUsers request
 
 module.exports = router;
